267e88b6f4855b1792c3546894618a1b6eced2f7
alanasim <alana.simoncelli@gmail.com>
2016-06-17 15:38:37 -0400

    working authorization

---
+++ app/controllers/application.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service(),
+
+  actions: {
+    authenticate: function(credentials) {
+      var authenticator = 'authenticator:jwt';
+      this.get('session').authenticate(authenticator, credentials).catch((reason)=>{
+        this.set('errorMessage', reason.responseJSON.errors || reason);
+      });
+    }
+  }
+});
