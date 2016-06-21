7e04fe51bb804aa0972cbb2b9812d3b73cdb89fd
alanasim <alana.simoncelli@gmail.com>
2016-06-21 09:53:10 -0400

    the art show page is prepped for sessionAccount access

--- app/routes/arts/art.js
+++ app/routes/arts/art.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
+const { service } = Ember.inject;
+
 export default Ember.Route.extend({
+  sessionAccount: service('session-account'),
+
+  beforeModel() {
+    return this._loadCurrentUser();
+  },
+
+  _loadCurrentUser() {
+    return this.get('sessionAccount').loadCurrentUser();
+  },
+
+  model(params) {
+    return this.store.findRecord('art', params["art_id"]);
+  }
 });
