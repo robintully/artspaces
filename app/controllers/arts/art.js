9d24bf32d656e2c008cfb114d86f3e328889bfbf
alanasim <alana.simoncelli@gmail.com>
2016-06-21 12:20:54 -0400

    edit functionality for art

--- app/controllers/arts/art.js
+++ app/controllers/arts/art.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  sessionAccount: Ember.inject.service()
+  sessionAccount: Ember.inject.service(),
+
+  actions: {
+    update: function(art) {
+      art.save().catch((e) => {
+          alert(e);
+      });
+    }
+  }
 });
