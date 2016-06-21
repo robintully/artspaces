4e38e1aff0ea30ec4302b7ff0bb6b9a9c6dad291
robintully <mr.robintully@gmail.com>
2016-06-20 23:07:44 -0400

    added delete

--- app/routes/arts/art.js
+++ app/routes/arts/art.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-});
+   actions: {
+     delete(resource) {
+       resource.deleteRecord();
+       resource.save();
+       this.transitionTo('arts');
+     }
+   }
+ });
